14a3f49160285e2a84e6d2a03c8238937d8bbd88
Sinclair <Sinclair@DESKTOP-NAD39Q3>
2018-12-27 15:30:57 -0500

    changes mad

--- src/leetCode/TwoSum.java
+++ src/leetCode/TwoSum.java
@@ -1,5 +1,8 @@
 package leetCode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
     /*
      * Given an array of integers, return indices of the two numbers such that they
@@ -14,6 +17,19 @@ public class TwoSum {
      * 
      * Because nums[0] + nums[1] = 2 + 7 = 9, return [0, 1].
      */
-    
-    
+
+    public int[] twoSum(int[] nums, int target) {
+	Map<Integer, Integer> map = new HashMap<>();
+	for (int i = 0; i < nums.length; i++) {
+	    map.put(nums[i], i);
+	}
+	for (int i = 0; i < nums.length; i++) {
+	    int complement = target - nums[i];
+	    if (map.containsKey(complement) && map.get(complement) != i) {
+		return new int[] { i, map.get(complement) };
+	    }
+	}
+	throw new IllegalArgumentException("No two sum solution");
+    }
+
 }
